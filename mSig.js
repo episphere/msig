7bc7df79cfb037b881e90b3d4cc198d3a9a63039
Jonas Almeida <jalmeida@mathbiol.org>
2022-11-18 10:49:39 -0500

    ini

---
+++ mSig.js
@@ -0,0 +1,4 @@
+(async function(){
+    mSig=await import('./methods.js')
+})()
+
---
+++ methods.js
@@ -0,0 +1,10 @@
+console.log(`methods.js called at ${Date()}`)
+
+
+function hello(){
+    console.log(`hello mSig at ${Date()}`)
+}
+
+export {
+    hello
+}
