2ed9916f6f94366dfa8ba554ba697ddf054b4e93
yasmmin <ycfrenchgirl2@gmail.com>
2023-04-08 12:08:48 -0300

    adding import to documentation examples

--- mSigSDKScripts/tcga.js
+++ mSigSDKScripts/tcga.js
@@ -16,8 +16,9 @@ let tcga = { 'mutspec': undefined }
 * @returns {Object} Object containing the list of projects in which the genes are over/under expressed and the list of projects organized by genes
 * 
 * @example
+* let tcga = await import('https://raw.githubusercontent.com/YasCoMa/msig/main/mSigSDKScripts/tcga.js')
 * let genes = ['ENSG00000155657']
-* var result = await getProjectsByGene(genes)
+* var result = await tcga.getProjectsByGene(genes)
 */
 async function getProjectsByGene(genes){
     var dat={}
@@ -54,24 +55,6 @@ async function getProjectsByGene(genes){
         }
     }
     
-    /*
-    for (var g of genes){
-        var url = `https://api.gdc.cancer.gov/analysis/top_cases_counts_by_genes?gene_ids=${g}`
-        var data = await fetch(url)
-        data = await data.json()
-        var temp=[]
-        for (var p of data['aggregations']['projects']['buckets']){
-            if( ! temp.includes(p['key']) ){
-                temp.push(p['key'])
-            }
-            if( ! projects.includes(p['key']) ){
-               projects.push(p['key'])
-            }
-        }
-        dat[g]=temp
-    }
-    */
-    
     var result = { 'projects': projects, 'projects_by_gene': dat }
     return result
 }
@@ -89,9 +72,10 @@ async function getProjectsByGene(genes){
 * @returns {Object} Object containing the list of count file ids and file descriptions organized by projects
 * 
 * @example
+* let tcga = await import('https://raw.githubusercontent.com/YasCoMa/msig/main/mSigSDKScripts/tcga.js')
 * let genes = ['ENSG00000155657']
 * let projects = ['TCGA-LUSC', 'TCGA-OV']
-* var result = await getTpmCountsByGenesOnProjects(genes, projects)
+* var result = await tcga.getTpmCountsByGenesOnProjects(genes, projects)
 */
 async function getTpmCountsByGenesOnProjects(genes, projects){
     var result = {}
@@ -152,51 +136,7 @@ async function getTpmCountsByGenesOnProjects(genes, projects){
         if (i >= ide.length) {
             break
         }
-    }/*
-    
-    for (var p of projects){
-        result[p] = {}
-        
-        var query = {
-            "filters": { 
-                "op":"and",
-                "content":[
-                    {
-                        "op":"in",
-                        "content":{
-                            "field":"cases.project.project_id",
-                                "value": [ p ]
-                        }
-                    },
-                    {
-                        "op":"=",
-                        "content":{
-                            "field":"data_type",
-                            "value":"Gene Expression Quantification"
-                        }
-                    },
-                    {
-                        "op":"=",
-                        "content":{
-                            "field":"experimental_strategy",
-                            "value":"RNA-Seq"
-                        }
-                    }
-                ]
-            },
-            "format": "tsv",
-            "fields": "file_id,file_name,cases.submitter_id,cases.case_id,data_category,data_type,cases.samples.tumor_descriptor,cases.samples.tissue_type,cases.samples.sample_type,cases.samples.submitter_id,cases.samples.sample_id,analysis.workflow_type,cases.project.project_id,cases.samples.portions.analytes.aliquots.submitter_id",
-            "size": "1000"
-        }
-        var data = await fetch( "https://api.gdc.cancer.gov/files", { method: 'POST', body: JSON.stringify(query), headers: { "Content-Type": "application/json" } } )
-        data = await data.text() 
-        var table = data.replace('\r','').split('\n').slice(1).map( e => { return e.split('\t') } )
-        var files = {}
-        var count_files = table.map( e => { files['workflow_type'] = e[0]; files['case_id'] = e[1]; files['sample_id'] = e[4]; files['sample_type'] = e[5]; files['cases_sample_submitter_id'] = e[6]; files['tissue_type'] = e[7]; files['tumor_descriptor'] = e[8]; files['cases_submitter_id'] = e[9]; files['data_category'] = e[10]; files['data_type'] = e[11]; files['file_name'] = e[13]; files['file_id'] = e[14]; return e[e.length-1]; } )
-        result[p]['count_files'] = count_files
-        result[p]['files_description'] = files
-        
-    }*/
+    }
     
     return result
 }
@@ -214,9 +154,10 @@ async function getTpmCountsByGenesOnProjects(genes, projects){
 * @returns {Object} Object containing the list of count ftpm and fpkm from each file organized by genes
 * 
 * @example
+* let tcga = await import('https://raw.githubusercontent.com/YasCoMa/msig/main/mSigSDKScripts/tcga.js')
 * let genes = ['ENSG00000155657']
 * let files = ['9e5f8edc-5074-43b7-a870-594aeb36e2aa', '8d5a94c8-b3d9-4991-8ce9-f7aa9189938c', 'dedf9f52-7ded-4cc5-bba2-da89a48b5176', '3aa53aa2-97cd-43a8-b7b1-09f0bf6381dd']
-* var result = await getTpmCountsByGenesFromFiles(genes, files)
+* var result = await tcga.getTpmCountsByGenesFromFiles(genes, files)
 */
 async function getTpmCountsByGenesFromFiles(genes, files){
     var result = {}
@@ -270,40 +211,6 @@ async function getTpmCountsByGenesFromFiles(genes, files){
             break
         }
     }
-    /*
-    var info = await Promise.all( files.map( async (f) => {    
-        var data = await fetch( `https://api.gdc.cancer.gov/data/${f}` )
-        data = await data.text() 
-        data=data.split('\n').map(e => { return e.split('\t') }).filter(e => e.length>1)
-        
-        var col_tpm=-1
-        var col_fpkm=-1
-        var i=0
-        for (var c of data[0]){
-            if( c.toLowerCase().indexOf('tpm')!=-1 ){
-                col_tpm = i
-            }
-            if( c.toLowerCase().indexOf('fpkm')!=-1 ){
-                col_fpkm = i
-            }
-            i+=1
-        }
-        
-        var gr=[]
-        var filter = data.filter(e => genes.includes( e[0].split('.')[0] ) )
-        filter.forEach( e => {
-            if(col_tpm!=-1 && col_fpkm!=-1){
-                var gene = e[0].split('.')[0]
-                result[gene]['name'] = e[1]
-                result[gene]['type'] = e[2]
-                result[gene]['counts_fpkm'][f] = e[col_fpkm]
-                result[gene]['counts_tpm'][f] = e[col_tpm]
-                gr.push([e[2], e[1], e[col_fpkm], e[col_tpm]])
-            }
-        })
-        return gr
-    } ) )
-    */
     
     return result
 }
@@ -320,8 +227,9 @@ async function getTpmCountsByGenesFromFiles(genes, files){
 * @returns {Object} Object containing the list of maf files and samples demographic information
 * 
 * @example
+* let tcga = await import('https://raw.githubusercontent.com/YasCoMa/msig/main/mSigSDKScripts/tcga.js')
 * let projects = ['TCGA-LUSC', 'TCGA-OV']
-* var result = await getMafInformationFromProjects(projects)
+* var result = await tcga.getMafInformationFromProjects(projects)
 */
 async function getMafInformationFromProjects( projects){
     var result = {}
@@ -407,8 +315,9 @@ async function getMafInformationFromProjects( projects){
 * @returns {Object} Object containing the list of patient mutation information
 * 
 * @example
+* let tcga = await import('https://raw.githubusercontent.com/YasCoMa/msig/main/mSigSDKScripts/tcga.js')
 * let res = { 'TCGA-LUSC': { 'maf_files': ['0b3d2db3-8ae3-4d39-bd9b-9d1e7a133b65', '9fed5902-6e95-4526-a119-ec4eade5576b' ] } }
-* var result = await getVariantInformationFromMafFiles(res)
+* var result = await tcga.getVariantInformationFromMafFiles(res)
 */
 async function getVariantInformationFromMafFiles(res){
     var result = {}
@@ -495,7 +404,8 @@ async function getVariantInformationFromMafFiles(res){
 * @param {string} url Library URL.
 * 
 * @example
-* loadScript('https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.11/pako.min.js')
+* let tcga = await import('https://raw.githubusercontent.com/YasCoMa/msig/main/mSigSDKScripts/tcga.js')
+* await tcga.loadScript('https://cdnjs.cloudflare.com/ajax/libs/pako/1.0.11/pako.min.js')
 *
 */
 async function loadScript (url){
@@ -525,4 +435,4 @@ if( typeof(mutspec)=="undefined" ){
     })
 }
 
-export { getProjectsByGene, getTpmCountsByGenesOnProjects, getTpmCountsByGenesFromFiles, getMafInformationFromProjects, getVariantInformationFromMafFiles }
+export { getProjectsByGene, getTpmCountsByGenesOnProjects, getTpmCountsByGenesFromFiles, getMafInformationFromProjects, getVariantInformationFromMafFiles, loadScript }
